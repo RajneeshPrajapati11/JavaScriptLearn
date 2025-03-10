706516f1ff7f1a65b0cc581705bf5c8127fa0d02
Rajneesh <130173363+RajneeshPrajapati11@users.noreply.github.com>
2025-03-10 16:19:16 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3+
+    3); //code readability hi kahtm hogyi isliyeh usko bhi yaad rakhna hai
+
+console.log("Rajneesh")
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint
+// string =>""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof "Rajneesh")
+console.log(typeof null)//important interview questions-- ek object hai
+console.log(typeof undefined)// iska type undefined hai
