8f30a4886bbcfe910a2cfba9047dc3494c4c0b28
Rajneesh <130173363+RajneeshPrajapati11@users.noreply.github.com>
2025-03-10 16:18:39 +0000

    variables

--- 01_basic/01_variables.js
+++ 01_basic/01_variables.js
@@ -1,9 +1,10 @@
 const accountId = 144553 // const ka matlab yeh hai ki ek baar jo value dedi ab vo lock hogyi means ab no change.
 
 /*variable ko declare karne k 2 tarike hai let and var but var use nahi krte because js mai ek chota sa problem hota tha ki isko scope nahi pata tha tohh hum scope mai jo variable ko define krte the tohh saare variable change hojaate the uss k basis pr */
-//{
+/*{
     //isko(currly braces ko) hi scope bolte hai
-//}
+}*/
+
 let accountEmail ="rajneesh@google.com"
 var accountPassword = "12345"
 accountCity = "Jaipur" // aisa bhi possible hai but please avoid such types of practices.
