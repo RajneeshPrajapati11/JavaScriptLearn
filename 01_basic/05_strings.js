b0586580035d993bf044ed8467107ad5753f95b0
Rajneesh <130173363+RajneeshPrajapati11@users.noreply.github.com>
2025-03-13 05:00:58 +0000

    srings

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,7 @@
+const name = "Rajneesh"
+const repoCount = 50
+
+console.log(name +repoCount+" value")//this syntax is outdated
+
+//syntax jo more readable hai
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`)//`` backtex// string interpolation bhi bolte hai
